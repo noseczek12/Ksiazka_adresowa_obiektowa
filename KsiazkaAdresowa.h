a9732a82fd1dca349ee95263df321326be70449a
Tomasz Nosol <tomasz.nosol.programista@gmail.com>
2020-10-09 19:00:57 +0200

    Added load user to vector on start program

--- KsiazkaAdresowa.h
+++ KsiazkaAdresowa.h
@@ -10,6 +10,7 @@ class KsiazkaAdresowa
 {
     UzytkownikMenedzer uzytkownikMenedzer;
 public:
+    KsiazkaAdresowa();
     void rejestracjaUzytkownika();
     void wypiszWszystkichUzytkownikow();
 };
--- PlikZUzytkownikami.h
+++ PlikZUzytkownikami.h
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <cstdlib>
 
 #include "Uzytkownik.h"
 #include "MetodyPomocnicze.h"
@@ -16,9 +17,11 @@ class PlikZUzytkownikami
     fstream plikTekstowy;
     bool czyPlikJestPusty();
     string zamienDaneUzytkownikaNaLinieZDanymiOddzielonaPionowymiKreskami(Uzytkownik uzytkownik);
+    Uzytkownik pobierzDaneUzytkownika(string daneJednegoUzytkownikaOddzielonePionowymiKreskami);
 public:
     PlikZUzytkownikami();
     void dopiszUzytkownikaDoPliku(Uzytkownik uzytkownik);
+    vector <Uzytkownik> wczytajUzytkownikowZPliku();
 };
 
 #endif
