1ff41cd4dc9086f1493b6e0e8d34cc6c5d23af26
Tomasz Nosol <tomasz.nosol.programista@gmail.com>
2020-10-12 11:07:53 +0200

    Implementation of user logging function

--- main.cpp
+++ main.cpp
@@ -8,9 +8,7 @@ int main()
 {
     KsiazkaAdresowa ksiazkaAdresowa("Uzytkownicy.txt");
     ksiazkaAdresowa.wypiszWszystkichUzytkownikow();
-    ksiazkaAdresowa.rejestracjaUzytkownika();
-    ksiazkaAdresowa.rejestracjaUzytkownika();
-    ksiazkaAdresowa.wypiszWszystkichUzytkownikow();
+    ksiazkaAdresowa.logowanieUzytkownika();
 
     return 0;
 }
